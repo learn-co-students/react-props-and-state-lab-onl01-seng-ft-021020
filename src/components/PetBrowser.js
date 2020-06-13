685dc267b2c4c6f7f1d983ca72e830cfeae4d81d
Lyoung0789 <Lestermyoung07@gmail.com>
2020-06-12 22:36:10 -0400

    Done.

--- src/components/PetBrowser.js
+++ src/components/PetBrowser.js
@@ -2,10 +2,24 @@ import React from 'react'
 
 import Pet from './Pet'
 
+
+
+
 class PetBrowser extends React.Component {
+
+//why do i need paranthesis instead of brackets????
+  genCards= () => ( 
+    this.props.pets.map(pet => {
+      return <Pet pet={pet} onAdoptPet={this.props.onAdoptPet} />
+    })
+  )
+
+
   render() {
-    return <div className="ui cards">PET COMPONENT SHOULD GO HERE</div>
+    return <div className="ui cards">{this.genCards()}</div>
   }
+
+
 }
 
 export default PetBrowser
